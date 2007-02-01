11652fe75fc50fd8cd0b8cc9002577a135079f66
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2007-02-01 17:53:36 +0000

    A função errorHandler não é mais necessária.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@60429 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lib/lua/luaunit-2.0.0/src/Test.lua
+++ lib/lua/luaunit-2.0.0/src/Test.lua
@@ -2,10 +2,6 @@ require "OOP"
 
 Test = createClass()
 
-function Test.errorHandler(errorMessage)
-  return debug.traceback(errorMessage, 2)
-end
-
 function Test:run(result)
   result:startTest(self.name)
   local _, errorMessage = pcall(self.test, self.testCase)
