21d8682442e6c69aeeee043e7361850ace389841
Ana Moura <moura@tecgraf.puc-rio.br>
2007-03-23 18:22:59 +0000

    # Interceptador cliente de requisiçoes de serviço
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@61811 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ src/components/common/ClientInterceptor.lua
@@ -0,0 +1,45 @@
+--
+-- Interceptador de requisições de serviço, responsável por inserir no
+--   contexto da requisição a credencial do cliente
+--
+local oil = require "oil"
+local oop = require "loop.base"
+
+local print = print
+
+module("ClientInterceptor", oop.class)
+
+-- Constrói o interceptador
+function __init(self, config, credentialHolder)
+
+  print("Construindo interceptador para cliente")
+  local lir = oil.getLIR()
+  return oop.rawnew(self, 
+                    {credentialHolder = credentialHolder,
+                     credentialType = lir:lookup_id(config.credential_type).type,
+                     contextID = config.contextID})
+end
+
+-- Intercepta o request para envio da informação de contexto (credencial)
+function sendrequest(self, request)
+  print ("INTERCEPTAÇÂO CLIENTE OP: "..request.operation)
+
+  -- Verifica de existe credencial para envio
+  if not self.credentialHolder:hasValue() then
+    print "SEM CREDENCIAL !"
+    return
+  end
+  print "TEM CREDENCIAL!"
+
+  -- Insere a credencial no contexto do serviço
+  local encoder = oil.newencoder()
+  encoder:put(self.credentialHolder:getValue(), 
+              self.credentialType)
+  request.service_context =  {
+    { context_id = self.contextID, context_data = encoder:getdata() }
+  }
+  print "INSERI CREDENCIAL"
+end
+
+function receivereply(reply)
+end
