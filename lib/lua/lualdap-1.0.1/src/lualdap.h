03281cd5d323e751e0b9543e2d27ebf202545ee2
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2008-04-04 17:26:37 +0000

    1. As bibliotecas lposix, luuid e lualdap foram removidas do repositório, pois passamos a utilizar as bibliotecas disponíveis no home do Tecgraf. 2. Biblioteca lce foi mantida, pois o código é nosso. Entretanto, foi movida para a raíz de lib.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@74583 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lib/lua/lualdap-1.0.1/src/lualdap.h
+++
@@ -1,8 +0,0 @@
-/*
-* lualdap.h
-*/
-
-#include "lua.h"
-#include "lualib.h"
-
-LUALIB_API int luaopen_lualdap (lua_State *L);
--- lib/lua/luuid/src/luuid.h
+++
@@ -1,8 +0,0 @@
-/*
-* luuid.h
-*/
-
-#include "lua.h"
-#include "lualib.h"
-
-LUALIB_API int luaopen_luuid(lua_State *L);
