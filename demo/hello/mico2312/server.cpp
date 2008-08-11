ac293b91bfe29af16e04a7c3a723bce2522b287c
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2008-08-11 17:35:52 +0000

    + Modificação no demo _hello_ do MICO afim de atender a tarefa OPENBUS-66 que alterou a chamada de método RGS:find().
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@81220 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/mico2312/server.cpp
+++ demo/hello/mico2312/server.cpp
@@ -11,81 +11,87 @@
 
 #include "hello.h"
 
-using namespace std ;
-using namespace openbusidl::acs ;
-using namespace openbusidl::rs ;
-using namespace openbus::common ;
+using namespace std;
+using namespace openbusidl::acs;
+using namespace openbusidl::rs;
+using namespace openbus::common;
 
-char* id ;
-IRegistryService_var rgs ;
+char* id;
+IRegistryService_var rgs;
 ORBInitializerImpl* ini;
-openbus::common::ServerInterceptor* serverInterceptor ;
+openbus::common::ServerInterceptor* serverInterceptor;
 
 class Hello_impl : virtual public POA_Hello {
   public:
     void sayHello() {
-      cout << endl<< "Servant diz: HELLO!" << endl ;
-      serverInterceptor = ini->getServerInterceptor() ;
+      cout << endl<< "Servant diz: HELLO!" << endl;
+      serverInterceptor = ini->getServerInterceptor();
       cout << "Usuário OpenBus que fez a chamada: " << serverInterceptor->getCredential()->entityName.in() << endl;
-    } ;
-} ;
+    };
+};
 
 CORBA::ORB_ptr orb = CORBA::ORB::_nil();
-Hello_impl* hello ;
-PortableServer::POA_var poa ;
+Hello_impl* hello;
+PortableServer::POA_var poa;
 
 class ORBThread : virtual public MICOMT::Thread {
   public:
-    void _run( void* )
+    void _run(void*)
     {
-      orb->run() ;
-      poa->destroy ( TRUE, TRUE ) ;
-      delete hello ;
+      orb->run();
+      poa->destroy (TRUE, TRUE);
+      delete hello;
     }
-} ;
+};
 
-int main( int argc, char* argv[] ) {
-  Lease lease = 0 ;
-  Credential_var credential ;
-  openbus::common::CredentialManager credentialManager ;
+int main(int argc, char* argv[]) {
+  Lease lease = 0;
+  Credential_var credential;
+  openbus::common::CredentialManager credentialManager;
 
-  ini = new ORBInitializerImpl( &credentialManager ) ;
-  PortableInterceptor::register_orb_initializer( ini ) ;
+  ini = new ORBInitializerImpl(&credentialManager);
+  PortableInterceptor::register_orb_initializer(ini);
 
-  orb = CORBA::ORB_init( argc, argv ) ;
+  orb = CORBA::ORB_init(argc, argv);
   CORBA::Object_var poaobj = orb->resolve_initial_references ("RootPOA");
   poa = PortableServer::POA::_narrow (poaobj);
   PortableServer::POAManager_var mgr = poa->the_POAManager();
 
-  CORBA::Object_var obj = orb->string_to_object( "corbaloc::localhost:2089/ACS" ) ;
-  IAccessControlService_var acs = IAccessControlService::_narrow( obj ) ;
+  CORBA::Object_var obj = orb->string_to_object("corbaloc::localhost:2089/ACS");
+  IAccessControlService_var acs = IAccessControlService::_narrow(obj);
 
-  bool status = acs->loginByPassword( "tester", "tester", credential, lease ) ;
-  if ( status ) {
-    credentialManager.setValue( credential ) ;
-    cout << "SERVER" << endl ;
-    cout << "Login efetuado no Openbus." << endl ;
-    cout << "entityName = " << credential->entityName.in() << endl ;
-    cout << "identifier = " << credential->identifier.in() << endl ;
+  bool status = acs->loginByPassword("tester", "tester", credential, lease);
+  if (status) {
+    credentialManager.setValue(credential);
+    cout << "SERVER" << endl;
+    cout << "Login efetuado no Openbus." << endl;
+    cout << "entityName = " << credential->entityName.in() << endl;
+    cout << "identifier = " << credential->identifier.in() << endl;
   } else {
-    return -1 ;
+    return -1;
   }
 
-  rgs = acs->getRegistryService() ;
-  hello = new Hello_impl ;
+  rgs = acs->getRegistryService();
+  hello = new Hello_impl;
 
-  ORBThread* main = new ORBThread ;
+  ORBThread* main = new ORBThread;
   main->start();
-  scs::core::IComponentImpl* c = new scs::core::IComponentImpl( "component", 1, orb, poa ) ;
-  c->addFacet( "facet", "IDL:Hello:1.0", hello ) ;
+  scs::core::IComponentImpl* c = new scs::core::IComponentImpl("component", 1, orb, poa);
+  c->addFacet("facet", "IDL:Hello:1.0", hello);
   mgr->activate ();
-  PropertyList p ;
-  ServiceOffer so ;
-  so.type = "type1" ;
-  so.description = "none" ;
-  so.properties = p ;
-  so.member = c->_this() ;
-  rgs->_cxx_register( so, id ) ;
-  main->wait() ;
-  return 0 ;
+  PropertyList_var p = new PropertyList(5);
+  p->length(1);
+  Property_var property = new Property;
+  property->name = "type";
+  PropertyValue_var propertyValue = new PropertyValue(5);
+  propertyValue->length(1);
+  propertyValue[(MICO_ULong) 0] = "type1";
+  property->value = propertyValue;
+  p[(MICO_ULong) 0] = property;
+  ServiceOffer so;
+  so.properties = p;
+  so.member = c->_this();
+  rgs->_cxx_register(so, id);
+  main->wait();
+  return 0;
 }
