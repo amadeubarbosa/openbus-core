ce9a72a850ac5c730c7d97e6df0c26ebe7cec585
Victor Sa Freire Fusco <vfusco@tecgraf.puc-rio.br>
2009-04-30 21:18:34 +0000

    Commit do item [OPENBUS-152]: Criar um repositório independente para o FTC
    Removendo a lib FTC do projeto OpenBus. 
    
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@91010 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lib/ftc/tests/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ksh
-
-. ${OPENBUS_HOME}/core/conf/config
-
-export LUA_PATH="?.lua;${OPENBUS_HOME}/?.lua;${OPENBUS_HOME}/core/utilities/lua/?.lua;${OPENBUS_HOME}/libpath/lua/5.1/?.lua;${OPENBUS_HOME}/libpath/lua/5.1/?/init.lua"
-
-exec ${OPENBUS_HOME}/core/bin/$TEC_UNAME/servicelauncher runtests.lua $*
