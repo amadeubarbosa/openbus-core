287a6ec080599535e534495abbf85811eaf75650
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2009-06-26 02:36:39 +0000

    [OPENBUS-224] Modificacoes na demo Hello Lua para utilizar a nova API Lua.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@92962 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/lua/client.lua
+++ demo/hello/lua/client.lua
@@ -1,60 +1,37 @@
 --
 -- OpenBus Demo
--- consumer.lua
+-- client.lua
 --
 
 local oil = require "oil"
+oil.verbose:level(3)
+local openbus = require "openbus.Openbus"
 
-local orb = oil.init {
-                       flavor = "intercepted;corba;typed;cooperative;base",
-                       tcpoptions = {reuseaddr = true}
-                     }
+-- Inicialização do barramento
+openbus:resetAndInitialize("localhost", 2089, orbinit)
+local orb = openbus:getORB()
 
-oil.orb = orb
-
-local ClientInterceptor = require "openbus.common.ClientInterceptor"
-local CredentialManager = require "openbus.common.CredentialManager"
-
---  oil.verbose:level(0)
-orb:loadidlfile "../idl/hello.idl"
-
+-- Execução
 function main ()
-  local IDLPATH_DIR = os.getenv("IDLPATH_DIR")
-  if IDLPATH_DIR == nil then
-    io.stderr:write("A variavel IDLPATH_DIR nao foi definida.\n")
-    os.exit(1)
-  end
+  -- Carga da IDL Hello
+  orb:loadidlfile("../idl/hello.idl")
 
-  local idlfile = IDLPATH_DIR.."/registry_service.idl"
-  orb:loadidlfile(idlfile)
-  idlfile = IDLPATH_DIR.."/access_control_service.idl"
-  orb:loadidlfile(idlfile)
-
+  -- Conexão com o barramento e obtenção do componente HelloComponent
   local user = "tester"
   local password = "tester"
-
-  accessControlService = orb:newproxy("corbaloc::localhost:2089/ACS", 
-    "IDL:openbusidl/acs/IAccessControlService:1.0")
-
--- instala o interceptador de cliente
-  local DATA_DIR = os.getenv("OPENBUS_DATADIR")
-  local config = assert(loadfile(
-    DATA_DIR.."/conf/advanced/InterceptorsConfiguration.lua"))()
-  credentialManager = CredentialManager()
-  orb:setclientinterceptor(ClientInterceptor(config, credentialManager))
-
-  local success
-  success, credential = accessControlService:loginByPassword(user, password)
-  credentialManager:setValue(credential)
-
-  registryService = accessControlService:getRegistryService()
-
+  local registryService = openbus:connect(user, password)
+  if not registryService then
+    io.stderr:write("HelloClient: Erro ao conectar ao barramento.\n")
+    os.exit(1)
+  end
   local offers = registryService:find({"IHello"})
-  -- Assume que o publisher é o único serviço cadastrado.
-  SS = orb:narrow(offers[1].member, "IDL:scs/core/IComponent:1.0")
-  local facet = SS:getFacet("IDL:demoidl/hello/IHello:1.0")
-  hello = orb:narrow(facet, "IDL:demoidl/hello/IHello:1.0")
-  hello:sayHello()
+
+  -- Assume que HelloComponent é o único serviço cadastrado.
+  local helloComponent = orb:narrow(offers[1].member,
+    "IDL:scs/core/IComponent:1.0")
+  local helloFacet = helloComponent:getFacet("IDL:demoidl/hello/IHello:1.0")
+  helloFacet = orb:narrow(helloFacet, "IDL:demoidl/hello/IHello:1.0")
+  helloFacet:sayHello()
 end
 
 oil.main(function()
--- demo/hello/lua/server.lua
+++ demo/hello/lua/server.lua
@@ -1,101 +1,74 @@
 --
 -- OpenBus Demo
--- publisher.lua
+-- server.lua
 --
 
 local oil = require "oil"
-
-local orb = oil.init {
-                       flavor = "intercepted;corba;typed;cooperative;base",
-                       tcpoptions = {reuseaddr = true}
-                     }
-
-oil.orb = orb
-
-local ClientInterceptor = require "openbus.common.ClientInterceptor"
-local ServerInterceptor = require "openbus.common.ServerInterceptor"
-local CredentialManager = require "openbus.common.CredentialManager"
-
 local oop = require "loop.base"
+local openbus = require "openbus.Openbus"
+
+-- Inicialização do barramento
+openbus:resetAndInitialize("localhost", 2089, orbinit)
+local orb = openbus:getORB()
+
 local scs = require "scs.core.base"
 
---  oil.verbose:level(0)
-orb:loadidlfile "../idl/hello.idl"
+-- Implementação da Faceta IHello
+local Hello = oop.class {}
+function Hello:sayHello()
+  local user = openbus:getInterceptedCredential().owner
+  print "HELLO!\n\n"
+  print("O usuário OpenBus " .. user .. " requisitou a operação sayHello.")
+end
 
-oil.tasks:register(coroutine.create(function() return orb:run() end))
+-- Descrições do componente HelloComponent
+local facetDescriptions = {}
+facetDescriptions.IComponent = {
+  name = "IComponent",
+  interface_name = "IDL:scs/core/IComponent:1.0",
+  class = scs.Component
+}
+facetDescriptions.IMetaInterface = {
+  name = "IMetaInterface",
+  interface_name = "IDL:scs/core/IMetaInterface:1.0",
+  class = scs.MetaInterface
+}
+facetDescriptions.IHello = {
+  name = "IHello",
+  interface_name = "IDL:demoidl/hello/IHello:1.0",
+  class = Hello
+}
+local componentId = {
+  name = "HelloComponent",
+  major_version = 1,
+  minor_version = 0,
+  patch_version = 0,
+  platform_spec = ""
+}
 
+-- Execução
 function main ()
-  local IDLPATH_DIR = os.getenv("IDLPATH_DIR")
-  if IDLPATH_DIR == nil then
-    io.stderr:write("A variavel IDLPATH_DIR nao foi definida.\n")
-    os.exit(1)
-  end
+  -- Carga da IDL Hello
+  orb:loadidlfile("../idl/hello.idl")
 
-  local idlfile = IDLPATH_DIR.."/registry_service.idl"
-  orb:loadidlfile(idlfile)
-  idlfile = IDLPATH_DIR.."/access_control_service.idl"
-  orb:loadidlfile(idlfile)
+  -- Permite que o ORB comece a aguardar requisições
+  openbus:run()
 
+  -- Instanciação do componente HelloComponent
+  local component = scs.newComponent(facetDescriptions, {}, componentId)
+
+  -- Conexão com o barramento e registro do componente
   local user = "tester"
   local password = "tester"
-
-  accessControlService = orb:newproxy("corbaloc::localhost:2089/ACS", 
-    "IDL:openbusidl/acs/IAccessControlService:1.0")
-
-  -- instala o interceptador de cliente
-  local DATA_DIR = os.getenv("OPENBUS_DATADIR")
-  local config = assert(loadfile(
-    DATA_DIR.."/conf/advanced/InterceptorsConfiguration.lua"))()
-  credentialManager = CredentialManager()
-  orb:setclientinterceptor(ClientInterceptor(config, credentialManager))
-  serverInterceptor = ServerInterceptor(config, accessControlService)
-  orb:setserverinterceptor(serverInterceptor)
-
-  local success
-  success, credential = accessControlService:loginByPassword(user, password)
-  credentialManager:setValue(credential)
-
-  registryService = accessControlService:getRegistryService()
-
-  local registryIdentifier
-
-  local Hello = oop.class{}
-    function Hello:sayHello()
-      local user = serverInterceptor:getCredential().owner
-      print "HELLO!\n\n"
-      print(string.format(
-        "O usuário OpenBus %s requisitou a operação sayHello.", user))
-      registryService:unregister(registryIdentifier)
-    end
-    local facetDescriptions = {}
-    facetDescriptions.IComponent = {
-      name = "IComponent", 
-      interface_name = "IDL:scs/core/IComponent:1.0",
-      class = scs.Component
-    }
-    facetDescriptions.IMetaInterface = {
-      name = "IMetaInterface", 
-      interface_name = "IDL:scs/core/IMetaInterface:1.0", 
-      class = scs.MetaInterface
-    }
-    facetDescriptions.IHello = {
-      name = "IHello", 
-      interface_name = "IDL:demoidl/hello/IHello:1.0", 
-      class = Hello
-    }
-    local componentId = {
-      name = "Membro", 
-      major_version = 1, 
-      minor_version = 0, 
-      patch_version = 0, 
-      platform_spec = ""
-    }
-    local component = scs.newComponent(facetDescriptions, {}, componentId)
-    success, registryIdentifier = registryService:register({ properties = {}, 
-      member = component.IComponent})
-    print("*********************************************\n")
-    print("PUBLISHER\nServiço Hello registrado no barramento do OpenBus.\n")
-    print("*********************************************")
+  local registryService = openbus:connect(user, password)
+  if not registryService then
+    io.stderr:write("HelloServer: Erro ao conectar ao barramento.\n")
+    os.exit(1)
+  end
+  registryService:register({ properties = {}, member = component.IComponent})
+  print("*********************************************\n")
+  print("PUBLISHER\nServiço Hello registrado no barramento do OpenBus.\n")
+  print("*********************************************")
 end
 
 oil.main(function()
