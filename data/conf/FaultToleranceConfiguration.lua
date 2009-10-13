5ef4dd6c266b6a65582589368b89bfbe4be5cc94
Maíra Gatti <mgatti@tecgraf.puc-rio.br>
2009-10-13 20:55:57 +0000

    [OPENBUS-376]
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@97440 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ data/conf/FaultToleranceConfiguration.lua
@@ -0,0 +1,16 @@
+
+return {
+	hosts = {
+		 ACS = {"corbaloc::localhost:2089/ACS",
+				  "corbaloc::localhost:2090/ACS",
+				  "corbaloc::localhost:2091/ACS",
+			 	 "corbaloc::localhost:2092/ACS",
+		         } ,
+		 RS = {"corbaloc::localhost:2019/RS",
+				 "corbaloc::localhost:2020/RS",
+				 "corbaloc::localhost:2021/RS",
+			 	 "corbaloc::localhost:2022/RS",
+		       } ,
+	         },
+}
+
