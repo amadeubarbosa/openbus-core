3c6e295077f419e686f23604a55bf8677e4ade6a
Maíra Gatti <mgatti@tecgraf.puc-rio.br>
2009-11-16 21:53:22 +0000

    - [OPENBUS-461] Commit parcial. Somente contem o esqueleto para atualizacao de estado via ServerInterceptor. - [OPENBUS-377] Alguns ajustes de diretorio na api Java.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@98809 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ data/conf/RSFaultToleranceConfiguration.lua
@@ -0,0 +1,12 @@
+
+return {
+	hosts = {
+		 RS = {"corbaloc::localhost:2019/RS",
+				 "corbaloc::localhost:2020/RS",
+			 	 },
+		 FTRS = {"corbaloc::localhost:2019/FTRS",
+				 "corbaloc::localhost:2020/FTRS",
+	         },
+	    },
+}
+
