0e3f56015bd7743468e1d563ef9c38f90fb832e7
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-04-05 17:24:50 +0000

    Formatacao do codigo
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@103946 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/services/accesscontrol/FTAccessControlServiceMonitor.lua
+++ core/services/accesscontrol/FTAccessControlServiceMonitor.lua
@@ -47,14 +47,14 @@ module("core.services.accesscontrol.FTAccessControlServiceMonitor")
 FTACSMonitorFacet = oop.class{}
 
 function FTACSMonitorFacet:isUnix()
-    --TODO - Confirmar se vai manter assim
-	if os.execute("uname") == 0 then
-	--unix
-       return true
-	else
-	--windows
- 	   return false
-	end
+  --TODO - Confirmar se vai manter assim
+  if os.execute("uname") == 0 then
+    --unix
+    return true
+  else
+    --windows
+    return false
+  end
 end
 
 ---
@@ -66,27 +66,27 @@ function FTACSMonitorFacet:getService()
   local recep =  self.context.IReceptacles
   recep = Openbus:getORB():narrow(recep, "IDL:scs/core/IReceptacles:1.0")
   local status, conns = oil.pcall(recep.getConnections, recep,
-                                  "IFaultTolerantService")
+      "IFaultTolerantService")
   if not status then
-      log:error("Nao foi possivel obter o Serviço: " .. conns[1])
-      return nil
+    log:error("Nao foi possivel obter o Serviço: " .. conns[1])
+    return nil
   elseif conns[1] then 
-      local service = conns[1].objref
-      service = Openbus:getORB():narrow(service, "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
-      return service
+    local service = conns[1].objref
+    service = Openbus:getORB():narrow(service, "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
+    return service
   end
   log:error("Nao foi possivel obter o Serviço.")
   return nil
 end
 
 function FTACSMonitorFacet:connect()
-     Openbus:init(self.config.hostName, self.config.hostPort)
-     Openbus.isFaultToleranceEnable = false
-     Openbus:_setInterceptors()
-     -- autentica o monitor, conectando-o ao barramento
-     Openbus:connectByCertificate(self.context._componentId.name,
-        DATA_DIR.."/"..self.config.monitorPrivateKeyFile, 
-        DATA_DIR.."/"..self.config.accessControlServiceCertificateFile)
+  Openbus:init(self.config.hostName, self.config.hostPort)
+  Openbus.isFaultToleranceEnable = false
+  Openbus:_setInterceptors()
+  -- autentica o monitor, conectando-o ao barramento
+  Openbus:connectByCertificate(self.context._componentId.name,
+      DATA_DIR.."/"..self.config.monitorPrivateKeyFile, 
+      DATA_DIR.."/"..self.config.accessControlServiceCertificateFile)
 end
 
 
@@ -94,110 +94,101 @@ end
 --Monitora o servico de controle de acesso e cria uma nova replica se necessario.
 ---
 function FTACSMonitorFacet:monitor()
+  Log:faulttolerance("[Monitor SCA] Inicio")
+  local timeOut = assert(loadfile(DATA_DIR .."/conf/FTTimeOutConfiguration.lua"))()
 
-    Log:faulttolerance("[Monitor SCA] Inicio")
-    local timeOut = assert(loadfile(DATA_DIR .."/conf/FTTimeOutConfiguration.lua"))()
+  while true do
+    local reinit = false
+    local ok, res = self:getService().__try:isAlive()
+    Log:faulttolerance("[Monitor SCA] isAlive? "..tostring(ok)) 
 
-    while true do
-  
-	    local reinit = false
-
-        local ok, res = self:getService().__try:isAlive()  
-
-	    Log:faulttolerance("[Monitor SCA] isAlive? "..tostring(ok)) 
-
-	    --verifica se metodo conseguiu ser executado - isto eh, se nao ocoreu falha de comunicacao
-        if ok then
-			--se objeto remoto esta em estado de falha, precisa ser reinicializado
-			if not res then
-			    reinit = true
-				Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso em estado de falha. Matando o processo...")
-				--pede para o objeto se matar
-                self:getService():kill()
-			end
-	    else
-			Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso nao esta disponivel...")
-			-- ocorreu falha de comunicacao com o objeto remoto
-			reinit = true
-	    end
-
-        if reinit then
-           Openbus.credentialManager:invalidate()
-           Openbus.acs = nil
-           
-		   local timeToTry = 0
-
-		   repeat
-		
-			   if self.recConnId ~= nil then
-      			   local status, ftRecD = 
-		      			oil.pcall(self.context.IComponent.getFacet, self.context.IComponent, "IDL:scs/core/IReceptacles:1.0")
-
-      			   if not status then
-		      			print("[IReceptacles::IComponent] Error while calling getFacet(IDL:scs/core/IReceptacles:1.0)")
-				      	print("[IReceptacles::IComponent] Error: " .. ftRecD)
-					    return
-				   end
-				   ftRecD = Openbus:getORB():narrow(ftRecD)
-			
-				   local status, void = oil.pcall(ftRecD.disconnect, ftRecD, self.recConnId)
-				   if not status then
-					   print("[IReceptacles::IReceptacles] Error while calling disconnect")
-					   print("[IReceptacles::IReceptacles] Error: " .. void)
-					   return
-				   end
-			
-				   Log:faulttolerance("[Monitor SCA] disconnect executed successfully!")
-		
-			   end
-
-		       Log:faulttolerance("[Monitor SCA] Levantando Servico de Controle de Acesso...")
-
-			   --Criando novo processo assincrono
-			   if self:isUnix() then
-			   --os.execute(BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort..
-			   --						" &  > log_access_control_server-"..tostring(t)..".txt")
-				   os.execute(BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort)
-			   else
-			   --os.execute("start "..BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort..
-			   --						" > log_access_control_server-"..tostring(t)..".txt")
-				   os.execute("start "..BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort)
-			   end
-
-	           -- Espera alguns segundos para que dê tempo do SCA ter sido levantado
-	           os.execute("sleep ".. tostring(timeOut.monitor.sleep))
-
-               self.recConnId = nil
-			   self:connect()
-			   if Openbus:isConnected() then
-			       local ftacsService = Openbus.ft
-			       
-				   local ftRec = self.context.IReceptacles
-				   ftRec = Openbus:getORB():narrow(ftRec, "IDL:scs/core/IReceptacles:1.0")
-				   self.recConnId = ftRec:connect("IFaultTolerantService",ftacsService)
-				   if not self.recConnId then
-					   Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTACSMonitor")
-					   os.exit(1)
-				   end
-			   else
-			      Log:faulttolerance("[Monitor SCA] Não conseguiu levantar ACS de primeira porque porta está bloqueada.")
-			      Log:faulttolerance("[Monitor SCA] Espera " .. tostring(timeOut.monitor.sleep) .." segundos......")
-				  os.execute("sleep ".. tostring(timeOut.monitor.sleep))
-			   end			   
-
-			   timeToTry = timeToTry + 1
-
-		   until self.recConnId ~= nil or timeToTry == timeOut.monitor.MAX_TIMES
-		    
-		   if self.recConnId == nil then
-		     Log:error("[Monitor SCA] Servico de controle de acesso nao pode ser levantado.")
-		     os.exit(1)
-		   end
-
-	       Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso criado.")
-
-        end
+    --verifica se metodo conseguiu ser executado - isto eh, se nao ocoreu falha de comunicacao
+    if ok then
+      --se objeto remoto esta em estado de falha, precisa ser reinicializado
+      if not res then
+        reinit = true
+        Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso em estado de falha. Matando o processo...")
+        --pede para o objeto se matar
+        self:getService():kill()
+      end
+    else
+      Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso nao esta disponivel...")
+      -- ocorreu falha de comunicacao com o objeto remoto
+      reinit = true
     end
+
+    if reinit then
+      Openbus.credentialManager:invalidate()
+      Openbus.acs = nil
+      local timeToTry = 0
+
+      repeat
+        if self.recConnId ~= nil then
+          local status, ftRecD = oil.pcall(self.context.IComponent.getFacet, 
+              self.context.IComponent, "IDL:scs/core/IReceptacles:1.0")
+
+          if not status then
+            print("[IReceptacles::IComponent] Error while calling getFacet(IDL:scs/core/IReceptacles:1.0)")
+            print("[IReceptacles::IComponent] Error: " .. ftRecD)
+            return
+          end
+          ftRecD = Openbus:getORB():narrow(ftRecD)
+
+          local status, void = oil.pcall(ftRecD.disconnect, ftRecD, self.recConnId)
+          if not status then
+            print("[IReceptacles::IReceptacles] Error while calling disconnect")
+            print("[IReceptacles::IReceptacles] Error: " .. void)
+            return
+          end
+
+          Log:faulttolerance("[Monitor SCA] disconnect executed successfully!")
+        end
+
+        Log:faulttolerance("[Monitor SCA] Levantando Servico de Controle de Acesso...")
+
+        --Criando novo processo assincrono
+        if self:isUnix() then
+          --os.execute(BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort..
+          --           " &  > log_access_control_server-"..tostring(t)..".txt")
+          os.execute(BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort)
+        else
+          --os.execute("start "..BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort..
+          --           " > log_access_control_server-"..tostring(t)..".txt")
+          os.execute("start "..BIN_DIR.."/run_access_control_server.sh --port=".. self.config.hostPort)
+        end
+
+        -- Espera alguns segundos para que dê tempo do SCA ter sido levantado
+        os.execute("sleep ".. tostring(timeOut.monitor.sleep))
+
+        self.recConnId = nil
+        self:connect()
+        if Openbus:isConnected() then
+          local ftacsService = Openbus.ft
+
+          local ftRec = self.context.IReceptacles
+          ftRec = Openbus:getORB():narrow(ftRec, "IDL:scs/core/IReceptacles:1.0")
+          self.recConnId = ftRec:connect("IFaultTolerantService",ftacsService)
+          if not self.recConnId then
+            Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTACSMonitor")
+            os.exit(1)
+          end
+        else
+          Log:faulttolerance("[Monitor SCA] Não conseguiu levantar ACS de primeira porque porta está bloqueada.")
+          Log:faulttolerance("[Monitor SCA] Espera " .. tostring(timeOut.monitor.sleep) .." segundos......")
+          os.execute("sleep ".. tostring(timeOut.monitor.sleep))
+        end
+
+        timeToTry = timeToTry + 1
+      until self.recConnId ~= nil or timeToTry == timeOut.monitor.MAX_TIMES
+
+      if self.recConnId == nil then
+        Log:error("[Monitor SCA] Servico de controle de acesso nao pode ser levantado.")
+        os.exit(1)
+      end
+
+      Log:faulttolerance("[Monitor SCA] Servico de Controle de Acesso criado.")
+    end
+  end
 end
 
 --------------------------------------------------------------------------------
@@ -212,7 +203,7 @@ end
 function startup(self)
   local monitor = self.context.IFTServiceMonitor
   monitor:connect()
-      
+
   if not Openbus:isConnected() then
     Log:error("Erro ao se logar no ACS")
     os.exit(1)
@@ -222,11 +213,10 @@ function startup(self)
   ftRec = Openbus:getORB():narrow(ftRec, "IDL:scs/core/IReceptacles:1.0")
   local connId = ftRec:connect("IFaultTolerantService",Openbus.ft)
   if not connId then
-	Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTACSMonitor")
+    Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTACSMonitor")
     os.exit(1)
   end
   
   monitor.recConnId = connId
-  
   Log:init("Monitor do servico de controle de acesso iniciado com sucesso")
 end
--- core/services/registry/FTRegistryServiceMonitor.lua
+++ core/services/registry/FTRegistryServiceMonitor.lua
@@ -62,158 +62,150 @@ function FTRSMonitorFacet:getService()
   local recep =  self.context.IReceptacles
   recep = Openbus:getORB():narrow(recep, "IDL:scs/core/IReceptacles:1.0")
   local status, conns = oil.pcall(recep.getConnections, recep,
-                                  "IFaultTolerantService")
+      "IFaultTolerantService")
   if not status then
-      log:error("Nao foi possivel obter o Serviço: " .. conns[1])
-      return nil
+    log:error("Nao foi possivel obter o Serviço: " .. conns[1])
+    return nil
   elseif conns[1] then 
-      local service = conns[1].objref
-      service = Openbus:getORB():narrow(service, "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
-      return service
+    local service = conns[1].objref
+    service = Openbus:getORB():narrow(service, "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
+    return service
   end
   log:error("Nao foi possivel obter o Serviço.")
   return nil
 end
 
 function FTRSMonitorFacet:connect()
-     Openbus:init(self.config.accessControlServerHostName, 
-                  self.config.accessControlServerHostPort)
-     Openbus:_setInterceptors()
-     Openbus:enableFaultTolerance()
-     
-     -- autentica o monitor, conectando-o ao barramento
-     Openbus:connectByCertificate(self.context._componentId.name,
-        DATA_DIR.."/"..self.config.monitorPrivateKeyFile, 
-        DATA_DIR.."/"..self.config.accessControlServiceCertificateFile)
+  Openbus:init(self.config.accessControlServerHostName, 
+      self.config.accessControlServerHostPort)
+  Openbus:_setInterceptors()
+  Openbus:enableFaultTolerance()
+
+  -- autentica o monitor, conectando-o ao barramento
+  Openbus:connectByCertificate(self.context._componentId.name,
+      DATA_DIR.."/"..self.config.monitorPrivateKeyFile, 
+      DATA_DIR.."/"..self.config.accessControlServiceCertificateFile)
 end
 
 
 function FTRSMonitorFacet:isUnix()
 --TODO - Confirmar se vai manter assim
-	if os.execute("uname") == 0 then
-	--unix
-           return true
-	else
-	--windows
- 	   return false
-	end
+  if os.execute("uname") == 0 then
+    --unix
+    return true
+  else
+    --windows
+    return false
+  end
 end
 
 ---
 --Monitora o serviço de registro e cria uma nova réplica se necessário.
 ---
 function FTRSMonitorFacet:monitor()
+  Log:faulttolerance("[Monitor SR] Inicio")
+  local timeOut = assert(loadfile(DATA_DIR .."/conf/FTTimeOutConfiguration.lua"))()
 
-    Log:faulttolerance("[Monitor SR] Inicio")
-    local timeOut = assert(loadfile(DATA_DIR .."/conf/FTTimeOutConfiguration.lua"))()
+  while true do  
+    local reinit = false
+    local ok, res = self:getService().__try:isAlive()  
+    Log:faulttolerance("[Monitor SR] isAlive? "..tostring(ok))  
 
-    while true do
-  
-	  local reinit = false
+    --verifica se metodo conseguiu ser executado - isto eh, se nao ocoreu falha de comunicacao
+    if ok then
+      --se objeto remoto esté em estado de falha, precisa ser reinicializado
+      if not res then
+        reinit = true
+        Log:faulttolerance("[Monitor SR] Servico de registro em estado de falha. Matando o processo...")
+        --pede para o objeto se matar
+        self:getService():kill()
+      end
+    else
+      Log:faulttolerance("[Monitor SR] Servico de registro nao esta disponivel...")
+      -- ocorreu falha de comunicacao com o objeto remoto
+      reinit = true
+    end
 
-      local ok, res = self:getService().__try:isAlive()  
-	  Log:faulttolerance("[Monitor SR] isAlive? "..tostring(ok))  
+    if reinit then
+      Openbus.credentialManager:invalidate()
+      Openbus.acs = nil
+      local timeToTry = 0
 
-	  --verifica se metodo conseguiu ser executado - isto eh, se nao ocoreu falha de comunicacao
-      if ok then
-	    --se objeto remoto esté em estado de falha, precisa ser reinicializado
-	    if not res then
-		    reinit = true
-	        Log:faulttolerance("[Monitor SR] Servico de registro em estado de falha. Matando o processo...")
-		    --pede para o objeto se matar
-            self:getService():kill()
-	    end
-	  else
-        Log:faulttolerance("[Monitor SR] Servico de registro nao esta disponivel...")
-	    -- ocorreu falha de comunicacao com o objeto remoto
- 	    reinit = true
-	  end
+      repeat
+        if self.recConnId ~= nil then
+          local status, ftRecD = oil.pcall(self.context.IComponent.getFacet, 
+              self.context.IComponent, "IDL:scs/core/IReceptacles:1.0")
 
-      if reinit then
-        Openbus.credentialManager:invalidate()
-        Openbus.acs = nil
-		local timeToTry = 0
+          if not status then
+            print("[IReceptacles::IComponent] Error while calling getFacet(IDL:scs/core/IReceptacles:1.0)")
+            print("[IReceptacles::IComponent] Error: " .. ftRecD)
+            return
+          end
+          ftRecD = Openbus:getORB():narrow(ftRecD)
+      
+          local status, void = oil.pcall(ftRecD.disconnect, ftRecD, self.recConnId)
+          if not status then
+            print("[IReceptacles::IReceptacles] Error while calling disconnect")
+            print("[IReceptacles::IReceptacles] Error: " .. void)
+            return
+          end
+          Log:faulttolerance("[Monitor SR] disconnect executed successfully!")
+        end
+        Log:faulttolerance("[Monitor SR] Levantando Servico de registro...")
 
-		repeat
-		
-		    if self.recConnId ~= nil then
-				local status, ftRecD = 
-					oil.pcall(self.context.IComponent.getFacet, self.context.IComponent, "IDL:scs/core/IReceptacles:1.0")
+        --Criando novo processo assincrono
+        if self:isUnix() then
+          os.execute(BIN_DIR.."/run_registry_server.sh --port=".. 
+              self.config.registryServerHostPort)
+          --os.execute(BIN_DIR.."/run_registry_server.sh --port=".. 
+          --self.config.registryServerHostPort.. 
+          --" & > log_registry_server-"..tostring(t)..".txt")
+        else
+          os.execute("start "..BIN_DIR.."/run_registry_server.sh --port="..
+              self.config.registryServerHostPort)
+          --os.execute("start "..BIN_DIR.."/run_registry_server.sh --port="..
+          --self.config.registryServerHostPort..
+          --"> log_registry_server-"..tostring(t)..".txt")
+        end
 
-				if not status then
-					print("[IReceptacles::IComponent] Error while calling getFacet(IDL:scs/core/IReceptacles:1.0)")
-					print("[IReceptacles::IComponent] Error: " .. ftRecD)
-					return
-				end
-				ftRecD = Openbus:getORB():narrow(ftRecD)
-			
-				local status, void = oil.pcall(ftRecD.disconnect, ftRecD, self.recConnId)
-				if not status then
-					print("[IReceptacles::IReceptacles] Error while calling disconnect")
-					print("[IReceptacles::IReceptacles] Error: " .. void)
-					return
-				end
-			
-				Log:faulttolerance("[Monitor SR] disconnect executed successfully!")
-			
-			end
+        -- Espera alguns segundos para que dê tempo do SR ter sido levantado
+        os.execute("sleep ".. tostring(timeOut.monitor.sleep))
 
-			Log:faulttolerance("[Monitor SR] Levantando Servico de registro...")
-				
-                                      
-			--Criando novo processo assincrono
-			
-			if self:isUnix() then
- 			    os.execute(BIN_DIR.."/run_registry_server.sh --port=".. self.config.registryServerHostPort)
- 			    --os.execute(BIN_DIR.."/run_registry_server.sh --port=".. self.config.registryServerHostPort..
-			--						" & > log_registry_server-"..tostring(t)..".txt")
-			else
-  			    os.execute("start "..BIN_DIR.."/run_registry_server.sh --port="..
-							 self.config.registryServerHostPort)
-  			    --os.execute("start "..BIN_DIR.."/run_registry_server.sh --port="..
-		--					 self.config.registryServerHostPort..
-		--					"> log_registry_server-"..tostring(t)..".txt")
-			end
+        self.recConnId = nil
+        self:connect()
+        if Openbus:isConnected() then
+          local rs = Openbus:getRegistryService()
+          local rsIC = rs:_component()
+          rsIC = Openbus:getORB():narrow(rsIC, "IDL:scs/core/IComponent:1.0")
+          local ftrsService = rsIC:getFacetByName("IFaultTolerantService")
+          ftrsService = Openbus:getORB():narrow(ftrsService, 
+              "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
 
-        	-- Espera alguns segundos para que dê tempo do SR ter sido levantado
-        	os.execute("sleep ".. tostring(timeOut.monitor.sleep))
-        	
-            self.recConnId = nil
-			self:connect()
-			if Openbus:isConnected() then
-			   local rs = Openbus:getRegistryService()
-			   local rsIC = rs:_component()
-               rsIC = Openbus:getORB():narrow(rsIC, "IDL:scs/core/IComponent:1.0")
-               local ftrsService = rsIC:getFacetByName("IFaultTolerantService")
-			   ftrsService = Openbus:getORB():narrow(ftrsService, 
-			         "IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
-			
-			   if OilUtilities:existent(ftrsService) then
-				  local ftRec = self:getFacetByName("IReceptacles")
-				  ftRec = orb:narrow(ftRec)
-				  self.recConnId = ftRec:connect("IFaultTolerantService",ftrsService)
-				  if not self.recConnId then
-					Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTRSMonitor")
-					os.exit(1)
-				  end
-			   end
-			else
-			   Log:faulttolerance("[Monitor SR] Não conseguiu levantar RS de primeira porque porta está bloqueada.")
-			   Log:faulttolerance("[Monitor SR] Espera " .. tostring(timeOut.monitor.sleep) .." segundos......")
-			   os.execute("sleep ".. tostring(timeOut.monitor.sleep))
-			end
-			timeToTry = timeToTry + 1
+          if OilUtilities:existent(ftrsService) then
+            local ftRec = self:getFacetByName("IReceptacles")
+            ftRec = orb:narrow(ftRec)
+            self.recConnId = ftRec:connect("IFaultTolerantService",ftrsService)
+            if not self.recConnId then
+              Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTRSMonitor")
+              os.exit(1)
+            end
+          end
+        else
+          Log:faulttolerance("[Monitor SR] Não conseguiu levantar RS de primeira porque porta está bloqueada.")
+          Log:faulttolerance("[Monitor SR] Espera " .. tostring(timeOut.monitor.sleep) .." segundos......")
+          os.execute("sleep ".. tostring(timeOut.monitor.sleep))
+        end
+        timeToTry = timeToTry + 1
 
-		until self.recConnId ~= nil or timeToTry == timeOut.monitor.MAX_TIMES
-		    
-		if self.recConnId == nil then
-		     Log:error("[Monitor SR] Servico de registro nao encontrado.")
-		     os.exit(1)
-		end
+      until self.recConnId ~= nil or timeToTry == timeOut.monitor.MAX_TIMES
+      if self.recConnId == nil then
+         Log:error("[Monitor SR] Servico de registro nao encontrado.")
+         os.exit(1)
+      end
 
-	    Log:faulttolerance("[Monitor SR] Servico de registro criado.")
-      end --fim reinit
-    end --fim while
+      Log:faulttolerance("[Monitor SR] Servico de registro criado.")
+    end --fim reinit
+  end --fim while
 end
 
 --------------------------------------------------------------------------------
@@ -228,7 +220,7 @@ end
 function startup(self)
   local monitor = self.context.IFTServiceMonitor
   monitor:connect()
-      
+  
   if not Openbus:isConnected() then
     Log:error("Erro ao se logar no ACS")
     os.exit(1)
@@ -236,25 +228,24 @@ function startup(self)
 
   local ftRec = self.context.IReceptacles
   ftRec = Openbus:getORB():narrow(ftRec, "IDL:scs/core/IReceptacles:1.0")
-  
-  local hostAdd = monitor.config.registryServerHostName..
-                 ":".. tostring(monitor.config.registryServerHostPort)
 
-  local ftrsService = Openbus:getORB():newproxy(
-      "corbaloc::"..hostAdd.."/FTRS","IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
+  local hostAdd = monitor.config.registryServerHostName..
+      ":".. tostring(monitor.config.registryServerHostPort)
+
+  local ftrsService = Openbus:getORB():newproxy("corbaloc::"..hostAdd..
+      "/FTRS","IDL:tecgraf/openbus/fault_tolerance/v1_05/IFaultTolerantService:1.0")
   if ftrsService:_non_existent() then
-      Log:error("Servico de registro nao encontrado.")
-      os.exit(1)
+    Log:error("Servico de registro nao encontrado.")
+    os.exit(1)
   end
 
   local connId = ftRec:connect("IFaultTolerantService",ftrsService)
   if not connId then
-	Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTRSMonitor")
+    Log:error("Erro ao conectar receptaculo IFaultTolerantService ao FTRSMonitor")
     os.exit(1)
   end
-  
+
   monitor.recConnId = connId
-  
   Log:init("Monitor do servico de registro iniciado com sucesso")
 end
 
