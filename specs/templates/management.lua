7f3b49ae37211099c9e40dfad39a13cf5189354d
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-05-27 14:25:11 +0000

    [OPENBUS-775] o hook do management não funciona quando o usuário altera o endereço padrão do barramento
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@106157 ae0415b3-e90b-0410-900d-d0be9363c56b

--- specs/templates/management.lua
+++ specs/templates/management.lua
@@ -1,22 +1,27 @@
 
 
 messages = {
-  { name = "admLogin", 
+  { name = "admLogin",
     msg = "Login do administrador",
     type = "string",
     value = "",
   },
 }
 
-configure_action = function(answers, tempdir, util)
+configure_action = function(answers, path, util)
   if answers.admLogin == "" then
     print "[ERRO] Login do administrador não foi informado"
     return false
   end
 
-  os.execute(
-      tempdir .. "/specs/shell/subscribe-services.sh " .. answers.admLogin 
-      .. " " .. tempdir
+  local acsConfFile = path.."/data/conf/AccessControlServerConfiguration.lua"
+  assert(loadfile(acsConfFile))()
+
+  hostName = AccessControlServerConfiguration.hostName
+  hostPort = AccessControlServerConfiguration.hostPort
+
+  return os.execute(
+      path .. "/specs/shell/subscribe-services.sh " .. answers.admLogin
+      .. " " .. hostName .. " " .. hostPort .. " " .. path
       )
-  return true
 end
