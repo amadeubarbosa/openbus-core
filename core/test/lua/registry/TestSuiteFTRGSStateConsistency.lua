142f22d88a83d3fbae8c6532d67e3adbcf7bb7ca
Maíra Gatti <mgatti@tecgraf.puc-rio.br>
2010-06-07 20:33:55 +0000

    [OPENBUS-777] Criação de um teste unitário para o mecanismo de atualização do estado das ofertas entre réplicas do Serviço de Registro; O teste consiste em registrar um oferta em uma réplica, buscar em todas e garantir que se encontre. O mesmo procedimento é feito para o caso de descadastro de uma oferta. [OPENBUS-802] Configurei a duração do lease das credenciais das réplicas do ACS com o mesmo tempo da credencial do ACS, isto é, nunca expira; [Openbus-803] Bugfix na busca de uma credencial em outra réplica; + Melhorias de assertividade no teste do mecanismo de atualização do estado das credenciais para o ACS para garantir que existe mais de uma réplica configurada; + Remoção de prints no teste do Serviço de Registro.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/branches/OB_v1_05_2010_05_13@106617 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ core/test/lua/registry/TestSuiteFTRGSStateConsistency.lua
@@ -0,0 +1,167 @@
+--
+-- Testes unitários para mecanismo de consistência do estado do Serviço de Registro
+--
+--[[
+  Esses testes assumem que existem *pelo menos* duas (2) réplicas do RGS.
+--]]
+local table = table
+require "oil"
+local orb = oil.orb
+local Check = require "latt.Check"
+local Utils = require "openbus.util.Utils"
+local scs = require "scs.core.base"
+local oop = require "loop.base"
+
+local OPENBUS_HOME = os.getenv("OPENBUS_HOME")
+
+function loadidls(self)
+  local IDLPATH_DIR = os.getenv("IDLPATH_DIR")
+  if IDLPATH_DIR == nil then
+    io.stderr:write("A variavel IDLPATH_DIR nao foi definida.\n")
+    os.exit(1)
+  end
+  orb:loadidlfile(IDLPATH_DIR.."/v1_05/registry_service.idl")
+  orb:loadidlfile(IDLPATH_DIR.."/v1_05/access_control_service.idl")
+  orb:loadidlfile(IDLPATH_DIR.."/v1_04/registry_service.idl")
+  orb:loadidlfile(IDLPATH_DIR.."/v1_04/access_control_service.idl")
+  orb:loadidl("interface IHello_vft { };")
+end
+
+local beforeTestCaseFTRGS = dofile(OPENBUS_HOME .."/core/test/lua/registry/beforeTestCaseFTRGS.lua")
+local afterTestCase = dofile(OPENBUS_HOME .."/core/test/lua/accesscontrol/afterTestCase.lua")
+
+-------------------------------------------------------------------------------
+-- Informações sobre os componentes usados nos testes
+--
+
+-- Descrições
+
+local Hello_vft  = {
+  -- Descrição dos receptáculos
+  receptacles = {},
+  -- Descrição das facetas
+  facets = {
+    IComponent = {
+      name = "IComponent",
+      interface_name = "IDL:scs/core/IComponent:1.0",
+      class = scs.Component
+    },
+    IMetaInterface = {
+      name = "IMetaInterface",
+      interface_name = "IDL:scs/core/IMetaInterface:1.0",
+      class = scs.MetaInterface
+    },
+    IHello_vft = {
+      name = "IHello_vft",
+      interface_name = "IDL:IHello_vft:1.0",
+      class = oop.class({}),
+    },
+  },
+  -- ComponentId
+  componentId = {
+    name = "Hello_vft",
+    major_version = 1,
+    minor_version = 0,
+    patch_version = 0,
+    platform_spec = "",
+  },
+  -- Propriedades para o registro.
+  properties = {
+    {name = "type",        value = {"IHello FT"}},
+    {name = "description", value = {"IHello FT versão 1.0"}},
+    {name = "version",     value = {"1.0"}},
+    -- Teste de propriedade vazia
+    {name = "bugs",        value = {}},
+  },
+}
+
+
+
+Suite = {
+
+  Test1 = {
+    beforeTestCase = beforeTestCaseFTRGS,
+
+    afterTestCase = afterTestCase,
+
+    testOffersSincronization =  function(self)
+
+       Check.assertTrue(# self.ftconfig.hosts.RS > 1)
+
+       local acsComp = orb:newproxy("corbaloc::localhost:2089/openbus_v1_05",
+                                    "IDL:scs/core/IComponent:1.0")
+       local facet = acsComp:getFacet("IDL:tecgraf/openbus/core/v1_05/access_control_service/IAccessControlService:1.0")
+       local acsFacet = orb:narrow(facet, "IDL:tecgraf/openbus/core/v1_05/access_control_service/IAccessControlService:1.0")
+
+       -- Login do serviço para a realização do teste
+       local challenge = acsFacet:getChallenge(self.deploymentId)
+       local privateKey = lce.key.readprivatefrompemfile(self.testKeyFile)
+       challenge = lce.cipher.decrypt(privateKey, challenge)
+       cert = lce.x509.readfromderfile(self.acsCertFile)
+       challenge = lce.cipher.encrypt(cert:getpublickey(), challenge)
+       local succ
+       succ, self.credential, self.lease =
+       acsFacet:loginByCertificate(self.deploymentId, challenge)
+       self.credentialManager:setValue(self.credential)
+
+       -- Recupera o Serviço de Registro
+       local acsIComp = acsFacet:_component()
+       acsIComp = orb:narrow(acsIComp, "IDL:scs/core/IComponent:1.0")
+       local acsIRecept = acsIComp:getFacetByName("IReceptacles")
+       acsIRecept = orb:narrow(acsIRecept, "IDL:scs/core/IReceptacles:1.0")
+       local conns = acsIRecept:getConnections("RegistryServiceReceptacle")
+       local rsIComp1 = orb:narrow(conns[1].objref, "IDL:scs/core/IComponent:1.0")
+       rsFacet1 = rsIComp1:getFacetByName("IRegistryService_v" .. Utils.OB_VERSION)
+       rsFacet1 = orb:narrow(rsFacet1,
+          "IDL:tecgraf/openbus/core/v1_05/registry_service/IRegistryService:1.0")
+       --cadastra oferta na primeira réplica
+       local member = scs.newComponent(Hello_vft.facets, Hello_vft.receptacles,
+        Hello_vft.componentId)
+       -- Identificar local propositalmente
+       local success, registryIdentifier = rsFacet1.__try:register({
+        member = member.IComponent,
+        properties = Hello_vft.properties,
+       })
+       Check.assertTrue(success)
+       Check.assertNotNil(registryIdentifier)
+
+       --busca em todas as réplicas (tem que encontrar)
+       for connId,conn in pairs(conns) do
+           if type (conn) == "table" then
+                local rsIComp = orb:narrow(conns[connId].objref, "IDL:scs/core/IComponent:1.0")
+                local rsFacet = rsIComp:getFacetByName("IRegistryService_v" .. Utils.OB_VERSION)
+                rsFacet = orb:narrow(rsFacet,
+                "IDL:tecgraf/openbus/core/v1_05/registry_service/IRegistryService:1.0")
+
+                local offers = rsFacet:find({"IHello_vft"})
+                Check.assertFalse(nil, offers[1])
+           end
+       end
+       --descadastra oferta na primeira réplica
+       local success, ret, retRemote = rsFacet1.__try:unregister(registryIdentifier)
+       Check.assertTrue(success)
+       Check.assertTrue(ret)
+       Check.assertTrue(retRemote)
+
+       --busca oferta nas outras réplicas (nao deveria encontrar)
+       for connId,conn in pairs(conns) do
+           if type (conn) == "table" then
+                local rsIComp = orb:narrow(conns[connId].objref, "IDL:scs/core/IComponent:1.0")
+                local rsFacet = rsIComp:getFacetByName("IRegistryService_v" .. Utils.OB_VERSION)
+                rsFacet = orb:narrow(rsFacet,
+                "IDL:tecgraf/openbus/core/v1_05/registry_service/IRegistryService:1.0")
+
+                local offers = rsFacet:find({"IHello_vft"})
+                Check.assertTrue(nil, offers[1])
+           end
+       end
+
+       Check.assertTrue(acsFacet:logout(self.credential))
+       self.credentialManager:invalidate()
+
+    end,
+  },
+
+}
+
+return Suite
