0ce53d1216768312cae201534ea5c82d0343c312
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-09-16 14:10:54 +0000

    - Revertendo commit que nao deveria ter sido feito na tag e sim no branch.
    Comando:
    svn merge -r HEAD:108792 svn+ssh://marosas@ssh.tecgraf.puc-rio.br/svn/engdist/openbus/tags/OB_v1_05_00_2010_06_29 .
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/tags/OB_v1_05_00_2010_06_29@110220 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/services/registry/RegistryService_v1_04.lua
+++ core/services/registry/RegistryService_v1_04.lua
@@ -3,10 +3,6 @@
 local oop = require "loop.simple"
 local oil = require "oil"
 
-local pairs = pairs
-local type = type
-local string = string
-
 ---
 --Componente (membro) responsável pelo Serviço de Registro na versao 1.04.
 ---
@@ -19,8 +15,8 @@ module("core.services.registry.RegistryService_v1_04")
 RSFacet = oop.class{}
 
 function RSFacet:register(serviceOffer)
-  local status, ret = oil.pcall(self.context.IRegistryService.register,
-                                self.context.IRegistryService,
+  local status, ret = oil.pcall(self.context.IRegistryService.register, 
+                                self.context.IRegistryService, 
                                 serviceOffer)
   if not status then
     return false, ""
@@ -29,12 +25,12 @@ function RSFacet:register(serviceOffer)
 end
 
 function RSFacet:unregister(identifier)
-  return self.context.IRegistryService:unregister(identifier)
+  return self.context.IRegistryService:unregister(identifier) 
 end
 
 function RSFacet:update(identifier, properties)
-  local status, ret = oil.pcall(self.context.IRegistryService.update,
-                                self.context.IRegistryService,
+  local status, ret = oil.pcall(self.context.IRegistryService.update, 
+                                self.context.IRegistryService, 
                                 identifier, properties)
   if not status then
     return false
@@ -47,17 +43,6 @@ function RSFacet:find(facets)
 end
 
 function RSFacet:findByCriteria(facets, criteria)
-  -- Troca valor de component_id de "nome:100", para "nome:1.0.0".
-  for _,props in pairs(criteria) do
-    if type(props) == "table" and props.name == "component_id" then
-        local componentId = props.value[1]
-        local newCompId = componentId:gsub("(.+):(%d)(%d)(%d)","%1:%2.%3.%4")
-        if newCompId then
-          props.value[1] = newCompId
-        end
-    end
-  end
-
   return self.context.IRegistryService:findByCriteria(facets, criteria)
 end
 
