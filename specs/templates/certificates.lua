500a05a88f1f480f3b1ec22ebd45aed7bcac8463
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-12-07 12:04:56 +0000

    [OPENBUS-1133] Adicionar o arquivo de cadastro das entidades dos testes nos pacotes  - Organizando melhor as pastas do pacote.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@113389 ae0415b3-e90b-0410-900d-d0be9363c56b

--- specs/templates/certificates.lua
+++ specs/templates/certificates.lua
@@ -1,21 +1,26 @@
 
 
 messages = {
+  { name = "test",
+    msg = "Você deseja criar os certificados do teste ?",
+    type = "string",
+    value = "nao",
+  },
 }
 
 configure_action = function(answers, path, util)
   local installPath =  path
-  local openSSLGenerate = " ../shell/openssl-generate.ksh -n "
+  local openSSLGenerate = "../shell/openssl-generate.ksh -n "
   -- Criando chaves dos serviços básicos.
   os.execute(
-      "cd " .. installPath .. "/specs/management;" .. 
+      "cd " .. installPath .. "/specs/management;" ..
       openSSLGenerate .. "AccessControlService;" ..
       openSSLGenerate .. "RegistryService;" ..
       openSSLGenerate .. "SessionService;" ..
       openSSLGenerate .. "ACSMonitor;" ..
       openSSLGenerate .. "RGSMonitor;"
       )
-  
+
   -- Movendo as chaves privadas para o diretório correto.
   os.execute(
       "cd " .. installPath .. "/specs/management;" ..
@@ -24,8 +29,17 @@ configure_action = function(answers, path, util)
       )
 
   -- Criando chaves dos demos.
-  
+
   -- Criando chaves para os testes.
-  
+  if string.upper(answers.test):find("SIM") then
+    openSSLGenerate = "../../../specs/specs/shell/openssl-generate.ksh -n "
+
+    os.execute(
+        "cd " .. installPath .. "/core/test/management;" ..
+        openSSLGenerate .. "TesteBarramento;"
+        )
+  end
+
+
   return true
 end
