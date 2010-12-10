fd2bb49feafb3f0f2d4ca5a84eaebbf46865c899
Maíra Gatti <mgatti@tecgraf.puc-rio.br>
2010-12-10 23:53:03 +0000

    [Openbus-875] - O teste do Serviço de Registro foi decomposto em 6 suites de acordo com a operação a que se refere. - Cada suite pode ser executada separadamente e já inclui cadastro no management. - Criei um script (run_registry_service) para executá-las todas de uma só vez, inclusive com possibilidade de executar a suite do FT, bastando passar o parâmetro indicativo. Ou seja, a suite testRegistryService.lua não existe mais. - Deixei somente um testCase para o caso de teste de carga, a do registro padrão, não há necessidade de ter mais de um. - Separei as funções utilitárias em um módulo utils. - Comparei estas funções com algumas existentes no Utils do sdk-lua, porém elas são totalmente diferentes e, para não correr o risco de, modificá-las lá sem perceber o impacto nos testes, achei que esta seria a melhor solução. - Não retirei os testes citados na issue [OPENBUS-994]. É preciso rever considerando a nova decomposição.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@113566 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ core/test/lua/registry/registerTestCase.lua
@@ -0,0 +1,47 @@
+--
+-- Teste unitário da operacao 'register' do Serviço de Registro
+--
+--
+local ClientInterceptor = require "openbus.interceptors.ClientInterceptor"
+local CredentialManager = require "openbus.util.CredentialManager"
+
+local scs = require "scs.core.base"
+
+local Check = require "latt.Check"
+
+local OPENBUS_HOME = os.getenv("OPENBUS_HOME")
+local Before = dofile(OPENBUS_HOME .."/core/test/lua/registry/beforeTestCase.lua")
+local beforeTestCase = Before.beforeTestCase
+local afterTestCase = dofile(OPENBUS_HOME .."/core/test/lua/registry/afterTestCase.lua")
+
+-------------------------------------------------------------------------------
+
+Suite = {
+  Test1 = {
+    beforeTestCase = beforeTestCase,
+
+    afterTestCase = afterTestCase,
+
+    testRegister = function(self)
+      local member = scs.newComponent(self.Hello_v2.facets, self.Hello_v2.receptacles,
+        self.Hello_v2.componentId)
+      -- Identificar local propositalmente
+      local success
+      sucess, self.registryIdentifier = self.rgsProtected:register({
+        member = member.IComponent,
+        properties = self.Hello_v2.properties,
+      })
+      Check.assertTrue(success)
+      Check.assertNotNil(self.registryIdentifier)
+
+      local offers = self.registryService:find({self.Hello_v2.facets.IHello_v1.name})
+      Check.assertEquals(1, #offers)
+
+      local offers = self.registryService:find({self.Hello_v2.facets.IHello_v2.name})
+      Check.assertEquals(1, #offers)
+      --
+    end,
+  },
+}
+
+return Suite
