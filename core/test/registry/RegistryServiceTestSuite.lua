b70efaca85547d13ed2e41df9c9261c615d64a3e
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2011-05-18 12:40:16 +0000

    Commit parcial do item [OPENBUS-1304]: Incluir testes para o método localFind do Serviço de Registro  - corrigindo bug nos testes do RS
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@118019 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/test/registry/RegistryServiceTestSuite.lua
+++ core/test/registry/RegistryServiceTestSuite.lua
@@ -17,7 +17,7 @@ local afterTestCase = dofile("registry/afterTestCase.lua")
 --------------------------------------------------------------------------------
 
 -- Muda de array para hash
-function props2hash(self, props)
+local function props2hash(props)
   local hash = {}
   for _, prop in ipairs(props) do
     local values = {}
@@ -30,7 +30,7 @@ function props2hash(self, props)
 end
 
 -- Verifica se propsA contém propsB
-function contains(self, propsA, propsB)
+local function contains(propsA, propsB)
   for nameB, valuesB in pairs(propsB) do
     -- Verifica se a propriedade existe
     local valuesA = propsA[nameB]
@@ -48,12 +48,12 @@ function contains(self, propsA, propsB)
 end
 
 -- Verifica se duas propriedades são iguais
-function equalsProps(self, propsA, propsB)
-  local propsA = self:props2hash(propsA)
-  local propsB = self:props2hash(propsB)
+local function equalsProps(propsA, propsB)
+  local propsA = props2hash(propsA)
+  local propsB = props2hash(propsB)
   
   -- Só é verdade de as duas forem iguais
-  return (self:contains(propsA, propsB) and self:contains(propsB, propsA))
+  return (contains(propsA, propsB) and contains(propsB, propsA))
 end
 
 --------------------------------------------------------------------------------
