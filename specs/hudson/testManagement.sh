66eb26e5fa2f2a2a662ff980597f3eaad4566884
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2011-05-24 14:18:57 +0000

    Commit parcial do item [OPENBUS-1304]: Atualizar os testes unitários do Openbus para incluir os testes dos serviços que estão faltando.  - corrigindo o script de execução do teste de gerência do hudson para levantar o serviço de sessão.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@118228 ae0415b3-e90b-0410-900d-d0be9363c56b

--- specs/hudson/testManagement.sh
+++ specs/hudson/testManagement.sh
@@ -71,6 +71,27 @@ fi
 
 ###############################################################################
 
+echo "Iniciando Serviço de Sessão"
+daemonize -o ${SSSOUTFILE} -e ${SSSERRFILE} -p ${SSSPIDFILE} ${OPENBUS_HOME}/core/bin/run_session_server.sh
+sleep 10
+SSSPID=`cat ${SSSPIDFILE}`
+
+if ! ( kill -0 ${SSSPID} 1>/dev/null 2>&1 ) ;then
+  echo "==============================================================================="
+  echo "[ERRO] Falha ao iniciar o Serviço de Sessão"
+  ShowLog "ACS" ${ACSOUTFILE} ${ACSERRFILE}
+  ShowLog "RGS" ${RGSOUTFILE} ${RGSERRFILE}
+  ShowLog "SSS" ${SSSOUTFILE} ${SSSERRFILE}
+  kill -9 ${RGSPID}
+  kill -9 ${ACSPID}
+  rm -f ${ACSOUTFILE} ${ACSERRFILE} ${ACSPIDFILE}
+  rm -f ${RGSOUTFILE} ${RGSERRFILE} ${RGSPIDFILE}
+  rm -f ${SSSOUTFILE} ${SSSERRFILE} ${SSSPIDFILE}
+  exit 1
+fi
+
+###############################################################################
+
 cd ${OPENBUS_HOME}/core/test
 cp ${OPENBUS_HOME}/data/certificates/AccessControlService.crt .
 echo -e "\n\n\n\n\n\n\n" | ${WORKSPACE}/hudson/genkey.sh TesteBarramento
