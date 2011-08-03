fae41a1c87c53ac34fac15efe49a842d91938e61
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2011-08-03 11:26:36 +0000

    [OPENBUS-1490] Gerar tag OpenBus Core 1.5.3_0 e ajustar os descritores do puts para encontrar todos os SDKs e bibliotecas dependentes.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/tags/OB_v1_05_03_00_2011_08_03@120664 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/services/version.h
+++ core/services/version.h
@@ -1,5 +1,5 @@
 #ifndef OPENBUS_VERSION
-#define OPENBUS_VERSION "OB_HEAD"
+#define OPENBUS_VERSION "OB_v1_05_03_00_2011_08_03"
 #endif  
 
 #define OPENBUS_PROJECT "Openbus Project (http://www.tecgraf.puc-rio.br/openbus)\nVersion: "OPENBUS_VERSION
