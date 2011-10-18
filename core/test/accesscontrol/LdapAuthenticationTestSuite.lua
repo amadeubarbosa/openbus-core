ede4e30bc85069ac01310840642b1457b456b5c7
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2011-10-18 18:43:12 +0000

    [OPENBUS-1324] Ajustes nos testes unitários para exercitar todos os casos de testes: + usando LDAP_URL (protocol://host:port) ao inves do padrao host:port + testando conexoes LDAP, LDAP+StartTLS e LDAPS (over SSL)
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk@123283 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/test/accesscontrol/LdapAuthenticationTestSuite.lua
+++ core/test/accesscontrol/LdapAuthenticationTestSuite.lua
@@ -11,140 +11,218 @@ Suite = {
   Test1 = {
     beforeTestCase = function(self)
       -- servidor ldap
-      self.hostname = "ldap-teste"
-      self.port = ":389"
-      self.SSLPort = ":636"
+      self.url = "ldap://ldap-teste:389"
+      self.urlSSL = "ldaps://ldap-teste:636"
       self.user = "cn=teste,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=br"
       self.password = "teste"
     end,
     
     testValidConnection = function(self)
-      local host = self.hostname .. self.port
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, self.password, false)
+      local url = self.url
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, false)
       Check.assertTrue(success)
       Check.assertNotNil(connection)
       connection:close()
     end,
 
+    testInvalidUrlSyntax = function(self)
+      local url = "invalid:666" -- missing '<protocol>://' prefix
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, false)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("LuaLDAP: Error connecting to server (Bad parameter to an ldap routine)", errorMessage)
+    end,
+
     testInvalidHost = function(self)
-      local host = "invalid" .. self.port
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, self.password, false)
+      local url = "ldap://invalid"
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Can't contact LDAP server", errorMessage)
     end,
 
     testInvalidUser = function(self)
-      local host = self.hostname .. self.port
+      local url = self.url
       local user = "unknown"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, false)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid DN syntax", errorMessage)
     end,
 
     testInvalidUser2 = function(self)
-      local host = self.hostname .. self.port
+      local url = self.url
       local user = "cn=unknown,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=br"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, false)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testInvalidUser3 = function(self)
-      local host = self.hostname .. self.port
+      local url = self.url
       -- incluindo dc=com
       local user = "cn=teste,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=com,dc=br"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, false)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testInvalidPassword = function(self)
-      local host = self.hostname .. self.port
+      local url = self.url
       local password = "invalid"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, password, false)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testBlankPassword = function(self)
-      local host = self.hostname .. self.port
+      local url = self.url
       local password = ""
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, password, false)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Server is unwilling to perform", errorMessage)
     end,
 
     testNilPassword = function(self)
-      local host = self.hostname .. self.port
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, nil, false)
+      local url = self.url
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, nil, false)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Server is unwilling to perform", errorMessage)
+    end,
+
+    testValidConnectionTLS = function(self)
+      local url = self.url
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, true)
+      Check.assertTrue(success)
+      Check.assertNotNil(connection)
+      connection:close()
+    end,
+
+    testInvalidUserTLS = function(self)
+      local url = self.url
+      local user = "unknown"
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Invalid DN syntax", errorMessage)
+    end,
+
+    testInvalidUser2TLS = function(self)
+      local url = self.url
+      local user = "cn=unknown,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=br"
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Invalid credentials", errorMessage)
+    end,
+
+    testInvalidUser3TLS = function(self)
+      local url = self.url
+      -- incluindo dc=com
+      local user = "cn=teste,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=com,dc=br"
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Invalid credentials", errorMessage)
+    end,
+
+    testInvalidPasswordTLS = function(self)
+      local url = self.url
+      local password = "invalid"
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Invalid credentials", errorMessage)
+    end,
+
+    testBlankPasswordTLS = function(self)
+      local url = self.url
+      local password = ""
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Server is unwilling to perform", errorMessage)
+    end,
+
+    testNilPasswordTLS = function(self)
+      local url = self.url
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, nil, true)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Server is unwilling to perform", errorMessage)
     end,
 
     testValidConnectionSSL = function(self)
-      local host = self.hostname .. self.SSLPort
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, self.password, true)
+      local url = self.urlSSL
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, false)
       Check.assertTrue(success)
       Check.assertNotNil(connection)
       connection:close()
     end,
+    
+    testInvalidSettingSSL = function(self)
+      -- we cannot try to connect using a ldaps:// and setting StartTLS flag as true
+      local url = self.urlSSL
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, self.password, true)
+      Check.assertTrue(success)
+      Check.assertNil(connection)
+      Check.assertEquals("Operations error", errorMessage)
+    end,
 
     testInvalidUserSSL = function(self)
-      local host = self.hostname .. self.SSLPort
+      local url = self.urlSSL
       local user = "unknown"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, true)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid DN syntax", errorMessage)
     end,
 
     testInvalidUser2SSL = function(self)
-      local host = self.hostname .. self.SSLPort
+      local url = self.urlSSL
       local user = "cn=unknown,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=br"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, true)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testInvalidUser3SSL = function(self)
-      local host = self.hostname .. self.SSLPort
+      local url = self.urlSSL
       -- incluindo dc=com
       local user = "cn=teste,ou=usuarios,dc=tecgraf,dc=puc-rio,dc=com,dc=br"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, user, self.password, true)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, user, self.password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testInvalidPasswordSSL = function(self)
-      local host = self.hostname .. self.SSLPort
+      local url = self.urlSSL
       local password = "invalid"
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, password, true)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Invalid credentials", errorMessage)
     end,
 
     testBlankPasswordSSL = function(self)
-      local host = self.hostname .. self.SSLPort
+      local url = self.urlSSL
       local password = ""
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, password, true)
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, password, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Server is unwilling to perform", errorMessage)
     end,
 
     testNilPasswordSSL = function(self)
-      local host = self.hostname .. self.SSLPort
-      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, host, self.user, nil, true)
+      local url = self.urlSSL
+      local success, connection, errorMessage = oil.pcall(lualdap.open_simple, url, self.user, nil, false)
       Check.assertTrue(success)
       Check.assertNil(connection)
       Check.assertEquals("Server is unwilling to perform", errorMessage)
