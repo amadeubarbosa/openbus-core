b4c9ec22f6b9e486e4a2aaf8272c771b9cd51275
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2011-12-12 16:09:01 +0000

    [OPENBUS-1593] Geracao tag OpenBus 1.5.3_01 (serviços básicos).
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/tags/OB_v1_05_03_01_2011_12_12@124805 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/services/version.h
+++ core/services/version.h
@@ -1,5 +1,5 @@
 #ifndef OPENBUS_VERSION
-#define OPENBUS_VERSION "OB_v1_05_03_2011_07_28"
+#define OPENBUS_VERSION "OB_v1_05_03_01_2011_12_12"
 #endif  
 
 #define OPENBUS_PROJECT "Openbus Project (http://www.tecgraf.puc-rio.br/openbus)\nVersion: "OPENBUS_VERSION
