66efe23ba654de556c028d690dc8fd054e1e80a6
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2012-06-25 16:34:07 +0000

    [OPENBUS-1599] Criar suite de testes para o core e sdk-lua do OpenBus 2.0
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/core/trunk@130303 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/runall.sh
+++ test/runall.sh
@@ -2,26 +2,26 @@
 
 CONSOLE="${OPENBUS_HOME}/bin/busconsole"
 RUNNER="${CONSOLE} ${OPENBUS_HOME}/lib/lua/5.1/latt/ConsoleTestRunner.lua"
-TESTDIR=${OPENBUS_HOME}/test/openbus/test/core
+TESTDIR=${OPENBUS_HOME}/test
 
 LUACASES="\
-services/LoginDB \
-Protocol \
+openbus/test/core/services/LoginDB \
+openbus/test/core/Protocol \
 "
 for case in ${LUACASES}; do
 	echo -n "Test '${case}' ... "
-	${CONSOLE} ${TESTDIR}/${case}.lua
+	${CONSOLE} ${TESTDIR}/${case}.lua $@
 	echo "OK"
 done
 
 LATTCASES="\
-services/LoginRegistry \
-services/CertificateRegistry \
-services/EntityRegistry \
-services/OfferRegistry \
-admin/admin \
+openbus/test/core/services/LoginRegistry \
+openbus/test/core/services/CertificateRegistry \
+openbus/test/core/services/EntityRegistry \
+openbus/test/core/services/OfferRegistry \
+openbus/test/core/admin/admin \
 "
-#LDAPAuthentication
+#openbus/test/core/services/LDAPAuthentication
 for case in ${LATTCASES}; do
 	echo "LATT '${case}':"
 	${RUNNER} ${TESTDIR}/${case}.lua
