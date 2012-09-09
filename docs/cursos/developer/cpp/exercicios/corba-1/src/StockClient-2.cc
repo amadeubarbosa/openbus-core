4dc569715a6ab126f47520592b785cab1c700c56
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-09-09 14:56:14 +0000

    OPENBUS-2032 Implementar a solução do exercício CORBA Parte II em C++
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/core/tags/OB_v1_05_03_01_2011_12_12@133161 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ docs/cursos/developer/cpp/exercicios/corba-1/src/StockClient-2.cc
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <CORBA.h>
+
+#include "StockMarket.h"
+
+int main(int argc, char **argv) {
+  std::ifstream ior(".ior");
+  std::string sior;
+  ior >> sior;
+  ior.close();
+  
+  CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
+  CORBA::Object_var o = orb->string_to_object(sior.c_str());
+  
+  StockMarket::StockServer * stockServer = StockMarket::StockServer::_narrow(o);
+  if (CORBA::is_nil(o)) {
+    std::cout << "error: narrow()." << std::endl;
+    return -1;
+  }
+  
+  StockMarket::StockSymbolList_var l = stockServer->getStockSymbols();
+  for (CORBA::ULong i = 0; i < l->length(); ++i)
+    std::cout << l[i] << ": " << stockServer->getStockValue(l[i]) << std::endl;
+
+  try {
+    stockServer->getStockValue("XXX");
+    assert(false);
+  } catch (StockMarket::InvalidSymbol &e) {
+    std::cout << "error: InvalidSymbol" << std::endl;
+  }
+  
+  return 0;
+}
