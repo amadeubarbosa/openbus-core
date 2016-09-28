70fc492104d2df2251e317ea73053ca078104a39
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2016-09-28 12:24:00 -0300

    OPENBUS-3008 nova operação em validadores de senha
    O validador de senhas para testes passa a retornar false na função de validação
    e erro na função de finalização a fim de melhorar o teste de integração.

--- test/openbus/test/core/services/BadPasswordValidator.lua
+++ test/openbus/test/core/services/BadPasswordValidator.lua
@@ -1,7 +1,12 @@
 return function (configs)
-  return function (entity, password)
+  return 
+  function (entity, password) -- validate function
     if entity == "error" and password == "Oops!" then
       error("Oops!")
     end
+    return false, "BadEntitiesShouldNotPass!"
+  end,
+  function () -- finalize function
+    error("Oops!")
   end
 end
